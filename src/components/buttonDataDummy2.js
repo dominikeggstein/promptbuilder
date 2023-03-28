3c80ec82bdcdc579a756574537c8031d7abdef8f
dominikeggstein <doeg-signup@outlook.com>
2023-03-28 11:59:22 +0200

    cleanup

--- src/components/buttonDataDummy2.js
+++
@@ -1,8 +0,0 @@
-const buttonDataDummy2 = [
-    { title: "Dummy2 Thema", options: [{ type: "input" }] },
-    { title: "Dummy2 Aufgabe", options: ["Option1", "Option2", { type: "input" }], multipleChoice: false },
-    { title: "Dummy2 Stil", options: ["Option1", "Option2", { type: "input" }], multipleChoice: true },
-  ];
-  
-  export default buttonDataDummy2;
-  
